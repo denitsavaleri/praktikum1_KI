4067b14b7907acd8c6ca59f33919d4beca42b8da
denitsa <dennyza44@gmail.com>
2022-11-06 15:06:48 +0100

    done

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import random
-from typing import List
+from typing import List, Optional
 import tsplib95
 from termcolor import colored
 
-# define own typefor the test instance from tsplib
-TSP_INSTANCE =  tsplib95.models.Problem
+tsp_instance: Optional[tsplib95.models.Problem] = None
 
 # population size
 POPULATION_SIZE = 10
 
-def read_tsp_file(file: str) -> TSP_INSTANCE:
+def read_tsp_file(file: str):
     """
     read tsp test instance from tsplib
     return: problem class which has all nodes(=cities) and weights(=distances)
     """
-    return tsplib95.load(file)
+    global tsp_instance
+    tsp_instance = tsplib95.load(file)
 
 class Individual():
     # initialize
@@ -24,13 +24,13 @@ class Individual():
         # path = list of the cities which have been/can be visited
         self.path = path
 
-def get_cities_from_instance(tsp_instance: TSP_INSTANCE):
+def get_cities_from_instance():
     return list(tsp_instance.get_nodes())
 
-def get_distance_from_instance(tsp_instance: TSP_INSTANCE, city1: int, city2: int):
+def get_distance_from_instance(city1: int, city2: int):
     return tsp_instance.get_weight(city1,city2)
 
-def get_fitness(path: list[int], tsp_instance: TSP_INSTANCE):
+def get_fitness(path: list[int]):
     """
 
     :param path: the list of citites to visit
@@ -39,7 +39,7 @@ def get_fitness(path: list[int], tsp_instance: TSP_INSTANCE):
     """
     fitness = 0
     for i in range(len(path)-1):
-        fitness += get_distance_from_instance(tsp_instance,path[i],path[i+1])
+        fitness += get_distance_from_instance(path[i],path[i+1])
     return fitness
 
 def get_random_path(cities):
@@ -50,7 +50,7 @@ def get_random_path(cities):
     random.shuffle(cities)
     return cities
 
-def create_initial_population(cities, tsp_instance)->List[Individual]:
+def create_initial_population(cities)->List[Individual]:
     """
     population = list of individuals = list of solutions
     :param1 = list of cities to visit
@@ -62,20 +62,20 @@ def create_initial_population(cities, tsp_instance)->List[Individual]:
     for i in range(POPULATION_SIZE):
         # need to copy the list of cities because otherwise paths get overwritten
         path = get_random_path(cities.copy())
-        fitness = get_fitness(path, tsp_instance)
+        fitness = get_fitness(path)
         population.append(Individual(fitness, path))
-
-
-    # print("Population: ")
-    # for i in range(POPULATION_SIZE):
-    #     print(population[i].path, population[i].fitness);
     return population
 
-def evolutionary_algortihm(population: List[Individual], n_iterations: int):
+def evolutionary_algortihm(population: List[Individual], n_iterations: int,cities, mu, llambda, mutation_probability):
     """
-    while loop for many generations
+    loop for many generations
     """
-    pass
+    for i in range(n_iterations):
+        print()
+        print("Generation ", i+1)
+        population = plus_selection(mu,llambda,population,mutation_probability,cities)
+        print(f"Fitness of best individuum: {population[0].fitness}")
+
 
 def mutation(individual : Individual, mutation_probability: float) -> Individual:
     """
@@ -119,7 +119,7 @@ def convert_to_valid_path(path_child: List[int], cities: List[int]):
     return valid_path
 
 
-def recombination(individual1: Individual, individual2: Individual, cross_point: int, tsp_instance, cities) -> List[Individual]:
+def recombination(individual1: Individual, individual2: Individual, cross_point: int, cities) -> List[Individual]:
     """
 
     :param individual1: parent one
@@ -142,8 +142,8 @@ def recombination(individual1: Individual, individual2: Individual, cross_point:
     path_child1 = convert_to_valid_path(path_child1,cities)
     path_child2 = convert_to_valid_path(path_child2, cities)
 
-    fitness_child1  = get_fitness(path_child1,tsp_instance )
-    fitness_child2 = get_fitness(path_child2, tsp_instance)
+    fitness_child1  = get_fitness(path_child1)
+    fitness_child2 = get_fitness(path_child2)
     #print the offspring :D
     print("child1 path:",  path_child1, "Fitness: ", fitness_child1)
     print("child2 path: ", path_child2,"Fitness: ", fitness_child2)
@@ -157,7 +157,7 @@ def recombination(individual1: Individual, individual2: Individual, cross_point:
 def update_population_with_new_children(population, children):
     population.extend(children)
 
-def plus_selection(mu: int, llambda: int, population: List[Individual], mutation_probability,cities, tsp_instance) -> List[Individual]:
+def plus_selection(mu: int, llambda: int, population: List[Individual], mutation_probability,cities) -> List[Individual]:
     """
     1. choose best mu parents
     2. create lambda children from the mu best parents
@@ -171,74 +171,90 @@ def plus_selection(mu: int, llambda: int, population: List[Individual], mutation
 
 
     # 1. choose best mu parents
+    # sort ascending
     best_parents = sorted(population,key=lambda individuum: individuum.fitness)
     # fitness = distance --> low fitness better
+    #choose the best mu parents
     best_parents = best_parents[:mu]
 
     #show the best parents choice
     print("best_parents")
-    for parent in best_parents:
-        print(parent.path, parent.fitness)
+    print_population(best_parents)
     print()
 
     # 2. create lambda children from the mu best parents
     print("New Offspring")
     new_offspring = []
-    for i in range(llambda):
+    while len(new_offspring) < llambda:
         random_number = random.random()
         # if < 0.5 -> mutation, if >0.5 -> recombination
         if random_number<0.5:
             print ("Mutation")
-            mutated_ind = mutation(population[i],mutation_probability)
+            mutated_ind = mutation( population[random.randint(0,len(population)-1)],mutation_probability)
             new_offspring.append(mutated_ind)
         else:
             print("Recombination" )
             cross_point_random = random.randint(0, len(population))
-            recombinated_ind = recombination(population[i], population[random.randint(0,len(population)-1)],cross_point_random,tsp_instance,cities)
+            recombinated_ind = recombination(population[random.randint(0,len(population)-1)], population[random.randint(0,len(population)-1)],cross_point_random,cities)
             new_offspring.extend(recombinated_ind)
 
     # 3. choose mu best individuals from parents + children
     mu_lambda_together = best_parents + new_offspring
+    # sort ascending
     mu_best_individuals = sorted(mu_lambda_together,key=lambda individuum: individuum.fitness)
+    # choose the best mu - individuals
     mu_best_individuals = mu_best_individuals[:mu]
     return mu_best_individuals
 
 def print_population(population):
-    print("\nPopulation: \nPATH	 FITNESS VALUE\n")
     for i in range(len(population)):
         print(population[i].path, population[i].fitness)
     print()
 
 
-def main():
+def main(n_iteration,best_parents, n_children, mutation_probability):
     # tsp file load
     file = "C:/Daten/Studium Info/Informatik TH Köln/5.Sem/KI/P/tsp-Dateien/a280.tsp"
-    tsp_test_instance = read_tsp_file(file)
+    read_tsp_file(file)
     #test if tsp can be read
-    cities = get_cities_from_instance(tsp_test_instance)
-    distance_between_city1_city2 = get_distance_from_instance(tsp_test_instance,cities[0], cities[1])
-    print("Cities: ",cities)
-    print("Distance between first and second city: ", distance_between_city1_city2)
-
+    cities = get_cities_from_instance()
+    distance_between_city1_city2 = get_distance_from_instance(cities[0], cities[1])
+    # print("Cities: ",cities)
+    # print("Distance between first and second city: ", distance_between_city1_city2)
+    #
     # create population
-    population = create_initial_population(cities,tsp_test_instance)
-    print_population(population)
+    population = create_initial_population(cities)
+    # print("\n Initial Population: \nPATH	 FITNESS VALUE\n")
+    # print_population(population)
+    #
+    # # mutation
+    # mutation(population[0], 0.2)
+    #
+    # # recombination
+    # offspring = recombination(population[1],population[2], 3, tsp_test_instance,cities)
+    # # update the population with the new children
+    # update_population_with_new_children(population,offspring)
+    # print("Population after recombination")
+    # # still not 12
+    # print_population(population)
 
-    # mutation
-    mutation(population[0], 0.2)
-
-    # recombination
-    offspring = recombination(population[1],population[2], 3, tsp_test_instance,cities)
-    # update the population with the new children
-    update_population_with_new_children(population,offspring)
-    print("Population after recombination")
-    # still not 12
-    print_population(population)
-
-    print("Selektion")
+    #print("Selektion")
     # best 3 parents and 6 new children
-    plus_selection(3,6,population,0.2, cities, tsp_test_instance)
+    #plus_selection(3,6,population,0.2, cities, tsp_test_instance)
     # for i in range(len(res)):
     #     print(res[i].path, res[i].fitness)
+
+
+    print("The Evolutionary Process beginns:")
+    # Welchen Einfl uss haben die Parameter auf die Ergebnisse?
+    # mit mehr generationen bekommen wir besseren fitness da wir immer die besten auswählen
+    evolutionary_algortihm(population,n_iteration,cities,best_parents, n_children, mutation_probability)
 if __name__ == "__main__":
-    main()
+    # immer mit den selben random Zahlen
+    random.seed(41)
+    #main(10,3,4,0.2)
+    # more generations -> better fitness
+    # main(10,3,4,0.2)
+    # more best parents ->
+    main(10, 3, 4, 0.2)
+
